9fc2150702877b43eef55c0d5dfb042d85fced6b
mszalkowski <mac.szalkowski@gmail.com>
2022-07-08 20:32:41 +0200

    asynchronous test popover

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,12 +1,37 @@
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form, OverlayTrigger, Popover } from 'react-bootstrap';
 import { useState } from 'react';
+
+// const popover = (
+//   <Popover id='popover-basic'>
+//     <Popover.Header as='h3'>Popover right</Popover.Header>
+//     <Popover.Body>
+//       And here's some <strong>amazing</strong> content. It's very engaging.
+//       right?
+//     </Popover.Body>
+//   </Popover>
+// );
+//
+// const Example = () => (
+//   <OverlayTrigger trigger='click' placement='right' overlay={popover}>
+//     <Button variant='success'>Click me to see</Button>
+//   </OverlayTrigger>
+// );
 
 export const SummaryForm = () => {
   const [tcChecked, setTcChecked] = useState(false);
 
+  const popover = (
+    <Popover id='popover-basic'>
+      <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+    </Popover>
+  );
+
   const checkboxLabel = (
     <span>
-      I agree to <span style={{ color: 'blue' }}>Terms and Conditions</span>
+      I agree to
+      <OverlayTrigger trigger={'hover'} placement='right' overlay={popover}>
+        <span style={{ color: 'blue' }}>Terms and Conditions</span>
+      </OverlayTrigger>
     </span>
   );
 
--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,5 +1,9 @@
 import { SummaryForm } from '../SummaryForm';
-import { render, screen } from '@testing-library/react';
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 const checkboxLabel = 'I agree to Terms and Conditions';
@@ -25,4 +29,25 @@ test('checkbox check enable button', () => {
   userEvent.click(checkbox);
   expect(checkbox).not.toBeChecked();
   expect(submitButton).toBeDisabled();
+});
+
+test('popover responds to hover', async () => {
+  render(<SummaryForm />);
+
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i,
+  );
+
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+
+  expect(nullPopover).toBeNull();
+
+  userEvent.hover(termsAndConditions);
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  userEvent.unhover(termsAndConditions);
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(/no ice cream will actually be delivered/i),
+  );
 });
