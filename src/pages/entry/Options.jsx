8b5c394b5047fa2c9152f3354a2cb9a7652feb52
mszalkowski <mac.szalkowski@gmail.com>
2022-07-11 20:26:15 +0200

    error handlig tests

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -3,18 +3,23 @@ import axios from 'axios';
 import ScoopOption from './ScoopOption';
 import { Row } from 'react-bootstrap';
 import ToopingOption from './ToopingOption';
+import AlertBanner from '../common/AlertBanner';
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+
+  const [error, setError] = useState(false);
+
   useEffect(() => {
     axios
       .get(`http://localhost:3000/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((error) => {
-        //:TODO handle errors
-      });
+      .catch((error) => setError(true));
   }, [optionType]);
 
+  if (error) {
+    return <AlertBanner />;
+  }
   const ItemComponent = optionType === 'scoops' ? ScoopOption : ToopingOption;
 
   const optionItems = items.map((item) => (
